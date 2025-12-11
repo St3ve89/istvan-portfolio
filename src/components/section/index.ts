ca4163a03d9e63a827eebe9f67cd1f896bff80d5
Istvan Acs <istvan.acs89@gmail.com>
2025-12-11 11:56:26 +0000

    feat: add process section

--- src/components/section/index.ts
+++ src/components/section/index.ts
@@ -1,3 +1,6 @@
 export { Section } from "./section";
 export { SectionHeader } from "./section-header";
 export { AnimatedSection } from "./animated-section";
+export { HeroSection } from "./hero-section";
+export { ProcessSection } from "./process-section";
+export { ServicesSection } from "./services-section";
